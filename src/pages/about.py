2e42fdce8aaee73894226aea7edd40caacbdeaf0
Olaf Bochmann <OleBo@users.noreply.github.com>
2021-03-20 19:48:35 +0100

    Create about.py

---
+++ src/pages/about.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def main():
+    st.title("About")
+    st.info(
+        """
+        This app is maintained by Olaf Bochmann. You can learn more about me at
+        [OleBo.github.io](https://OleBo.github.io).
+        """
+)
